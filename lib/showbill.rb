bc2db964868ed934b6d6a2d35814b7d6c28ed233
Amy Fogelman <abfnj1@gmail.com>
2019-08-08 19:43:11 +0000

    added showbill.rb

---
+++ lib/showbill.rb
@@ -0,0 +1,7 @@
+require "open-uri"
+require "nokogiri"
+require "pry"
+
+require_relative './showbillgemCLI/version'
+require_relative './showbillgemCLI/cli'
+require_relative './showbillgemCLI/show'
