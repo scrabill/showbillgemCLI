65bab00c3a17bd16969452491b0c0b359faed8f9
Amy Fogelman <abfnj1@gmail.com>
2019-08-09 01:23:19 +0000

    added more to cli.rb

--- lib/showbillgemCLI/cli.rb
+++ lib/showbillgemCLI/cli.rb
@@ -1,3 +1,30 @@
+class Showbills::CLI
+
+def call
+  list_deals
+  menu
+end
+
+def list_shows
+  #here 
+  
+def menu
+  puts "Enter the number of the show you'd like more information on or type exit."
+  while input
+  
+  case input
+  when "1"
+     puts "More info on show 1"
+  when "2"
+     puts "More info on show 2"
+   end
+  end 
+end  
+     
+     
+
+
+
 elsif input == "list"
    list_shows
   else 
