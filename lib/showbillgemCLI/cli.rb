638c4ceb1816f61de7b81ae20110696dfcfc3a8d
Amy Fogelman <abfnj1@gmail.com>
2019-08-09 15:43:03 +0000

    modified cli.rb

--- lib/showbillgemCLI/cli.rb
+++ lib/showbillgemCLI/cli.rb
@@ -13,13 +13,17 @@ def list_shows
  doc
 end 
  
- 
 def menu
 input = nil
   while input != "exit"
   puts "Enter the number of the show you'd like more information on or type list or type exit."
   input = gets.strip.downcase
-  case input
+  if input.to_1 > 0
+  puts @shows[input.to_i-1]
+elsif input == "list"
+  list_shows
+else  
+  puts "Not sure which show you want to see? Type list or exit."
   when "1"
      puts "More info on show 1..."
   when "2"
@@ -37,12 +41,3 @@ def goodbye
      
 
 
-
-elsif input == "list"
-   list_shows
-  else 
-    puts "Invalid entry.\nType 'list' to list Broadway shows currently playing or type 'exit' to leave:" if input != "exit"
-   end
-  end
-end  
-  
