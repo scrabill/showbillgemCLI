31f4cc478b8d98a99916a23307c0db723e7cfd8d
Amy Fogelman <abfnj1@gmail.com>
2019-08-11 12:32:19 +0000

    modified showbillgem.rb

--- lib/showbillgemCLI/showbillgem.rb
+++ lib/showbillgemCLI/showbillgem.rb
@@ -11,7 +11,7 @@ class ShowbillScrape::Showbills
   def initialize
     @title = title
     @location = location
-    @text = text
+    @showtext = showtext
     @@all << self
     @url = Nokogiri::(open("http://www.playbill.com/productions?q=&venue-type=broadway&zip="))
   end
@@ -24,20 +24,19 @@ class ShowbillScrape::Showbills
   def self.scrape_shows
     doc = Nokogiri::HTML(open("http://www.playbill.com/productions?q=&venue-type=broadway&zip="))
     shows = doc.css
-    shows.map do |show|
       self.new(show.text.strip, "playbill.com"#{show.attr("href").strip}")
     end
   end
 
   def title
-    @showdoc = self.showdoc.css("div.pb-pl-tile-title").text.strip
+    @title = self.showdoc.css("div.pb-pl-tile-title").text.strip
   end
 
   def location
-    @theater = self.showdoc.css("div.pb-pl-tile-location").text.strip
+    @location = self.showdoc.css("div.pb-pl-tile-location").text.strip
   end
 
-  def text
+  def showtext
     @showtext = self.showdoc.css("div.bsp-bio-text").text.strip
   end
 end
