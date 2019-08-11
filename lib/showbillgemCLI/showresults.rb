4c3ea4667f98e6036da1e4d48fe1aa21f83f5329
Amy Fogelman <abfnj1@gmail.com>
2019-08-11 16:26:46 +0000

    modify showresults.rb

--- lib/showbillgemCLI/showresults.rb
+++ lib/showbillgemCLI/showresults.rb
@@ -1,9 +1,16 @@
- @@all
-end	
+class ShowbillgemCLI::Scraper
 
-def self.current
-  self.scrape_shows	
+def self.show_scraper
+  if ShowbillgemCLI::showbillgem.allempty?
+    doc = Nokogiri::HTML(open("http://www.playbill.com/productions?q=&venue-type=broadway&zip=)
+    array_showbillgem_list = doc.css("li")[1...50]
+    array_showbillgem_list.each do |showbillgem_attr|
+    showbillgem = ShowbillgemCLI::showbillgem.new
+    showbillgem.title = showbillgem_attr.css("a").children.first.text
+    showbillgem.url = "http://www.playbill.com" + showbillgem_attr.css("a").attribute("href").value
   end
+ end
+end
   
   def self.scrape_shows
     doc = Nokogiri::HTML(open(""http://www.playbill.com/productions?q=&venue-type=broadway&zip=""))	    
